510b48f4f41033fc261ed9b827d177c6b5deba2a
robyng <robyngraham01@gmail.com>
2021-10-04 14:15:38 -0700

    psuedo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,18 @@
-// Assignment code here
+//When generate password button is pressed then
 
+//ask how many characters do you need? Window prompt numbers between 8 and 128 only
+
+//then ask if they want special characters window confirm
+
+//then ask if they want upper case window confirm
+
+//then ask if they want lower case window confirm
+
+//then ask if they want numbers
+
+//if cancel is selected for all types then window confirm they must pick one 'ok' to start over 'cancel' to leave.
+
+//print their password in textarea
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
