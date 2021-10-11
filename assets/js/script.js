b33a2501aff6c1440b707656a2eaab4299fcc3f1
robyng <robyngraham01@gmail.com>
2021-10-10 23:08:45 -0700

    remove white space and old comments, add new comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,22 @@ lowerCaseSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 
 masterArray = []
 
-
-
-
-
 //When generate password button is pressed then ask how many characters
 
 generateBtn.addEventListener('click', function() { 
   
 
-
   var passLength = window.prompt('How long do you want your password? Enter a number between 8 and 128.');
-  if (passLength < 8 || passLength > 128 || isNaN(passLength) ) {
+  // loop for if input is not valid try again
+  while (passLength < 8 || passLength > 128 || isNaN(passLength) ) {
     window.alert('Try agian');
+    var passLength = window.prompt('How long do you want your password? Enter a number between 8 and 128.');
 
-    } else {
-      window.confirm('Thank you!');
+    } 
+ 
+    window.confirm('Thank you!');
 
-    }
-
-
-
+    
   var yesNumbers = window.confirm('Do you want numbers in your password?')
 
   var yesUppercase = window.confirm('Do you have want UPPERCASE letters in your password?')
@@ -40,7 +35,7 @@ generateBtn.addEventListener('click', function() {
   
   var askSpecialChar = window.confirm('Do you want to have special characters?');
 
-  //then ask if they want special characters window confirm
+  // if yes to options add them to the master array using concat
 
   if (yesNumbers) {
     masterArray = masterArray.concat(numberSet)
@@ -58,8 +53,8 @@ generateBtn.addEventListener('click', function() {
   if (askSpecialChar) {
     masterArray = masterArray.concat(specialCharSet)
   }
- 
 
+      // get random number from masterArray and iterate til length of chosen passLength 
       var newPass = ''
 
       for (var i = 0; i < passLength; i++) {
@@ -78,49 +73,8 @@ generateBtn.addEventListener('click', function() {
     }
     writePassword();
 
-      // make masterArray empty again so it doens't carry over if generate clicked again
+      // make masterArray empty again so it doesn't carry over if generate clicked again
       masterArray = []
 
 
     })
-  
-
-  
-
-
-
- 
-
-
-
-
-    
-    // var passwordText = document.querySelector("#password");
-
-    // passLength.textContent = passwordText;
-
-
-
-
-
-
-
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-//then ask if they want upper case window confirm
-
-//then ask if they want lower case window confirm
-
-//then ask if they want numbers
-
-//if cancel is selected for all types then window confirm they must pick one 'ok' to start over 'cancel' to leave.
-
-//print their password in textarea
-
-// Get references to the #generate element
